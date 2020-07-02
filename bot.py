d8ef144a98d7626f800a8886215cdd8f121c6eb5
Cryofracture <thecryoforce@gmail.com>
2020-07-02 03:24:14 -0500

    Pushing to github

--- bot.py
+++ bot.py
@@ -10,9 +10,22 @@ TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
 reddit = praw.Reddit('wholesome_porn_bot')
 
-posts = []
-for submission in reddit.subreddit('foodporn').hot(limit=50):
-    posts.append(submission.url)
+food_posts = []
+earth_posts = []
+library_posts = []
+aerial_posts = []
+
+for submission in reddit.subreddit('foodporn+culinaryporn+dessertporn').hot(limit=50):
+    food_posts.append(submission.url)
+
+for submission in reddit.subreddit('earthporn+waterporn+skyporn+seaporn').hot(limit=50):
+    earth_posts.append(submission.url)
+
+for submission in reddit.subreddit('libraryporn+bookporn').hot(limit=50):
+    library_posts.append(submission.url)
+
+for submission in reddit.subreddit('aerialporn').hot(limit=50):
+    aerial_posts.append(submission.url)
 
 client = discord.Client()
 
@@ -34,7 +47,19 @@ async def on_message(message):
 
 
     if message.content == '!foodporn':
-        response = random.choice(posts)
+        response = random.choice(food_posts)
         await message.channel.send(f'Wholesome food porn incoming!\n{response}')
+
+    if message.content == '!earthporn':
+        response = random.choice(earth_posts)
+        await message.channel.send(f'Wholesome earth porn incoming!\n{response}')
+
+    if message.content == '!libraryporn':
+        response = random.choice(library_posts)
+        await message.channel.send(f'Wholesome library porn incoming!\n{response}')
+
+    if message.content == '!aerialporn':
+        response = random.choice(aerial_posts)
+        await message.channel.send(f'Wholesome aerial porn incoming!\n{response}')
 
 client.run(TOKEN)
